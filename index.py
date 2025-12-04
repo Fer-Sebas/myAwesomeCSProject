f13a55bb446a0681fcf9834e5744b8f580c9ad89
Sebastian Ferreira <io@fersebas.com>
2025-12-04 14:46:12 -0500

    Very important changes I'll share with my teacher

--- index.py
+++ index.py
@@ -2,4 +2,24 @@
 
 # Ask at least 3 multiple choice questions
 
-# Show multiple
+# Show multiple-choice options for each question
+
+# Accept user's choice (A, B, C)
+
+# Check if the choice is correct
+
+# Tell the user if the answer was right or wrong
+# Clue: "if" this, do that, "else" do that other that
+
+# Keep track of the score
+# Clue: A "magic" box with a label
+
+# Show the final score
+
+score = 0
+
+# I got my question right (option 1)
+score = score + 1
+
+# I got my question right (option 2)
+score += 1
