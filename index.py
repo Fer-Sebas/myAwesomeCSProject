1288e099d9e8d1ef12445b08a2f6f8b94eb688c5
Sebastian Ferreira <io@fersebas.com>
2025-12-11 14:25:47 -0500

    important changes

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
 # Greet the user
+print(' \n Hi \n')
 
 # Ask at least 3 multiple choice questions
+print("What color is coffee?")
+print("A. Blue")
+print("B. Brown")
+print("C. Orange")
 
 # Show multiple-choice options for each question
 
 # Accept user's choice (A, B, C)
+
+answer1 = input("What is your answer?")
 
 # Check if the choice is correct
 
@@ -16,10 +23,10 @@
 
 # Show the final score
 
-score = 0
+# score = 0
 
 # I got my question right (option 1)
-score = score + 1
+# score = score + 1
 
 # I got my question right (option 2)
-score += 1
+# score += 1
